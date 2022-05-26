12dc06209625ebd574eb839628deb388f65f68e4
Rostislav Dmitriev <udesurd@gmail.com>
2022-05-26 20:02:09 +0300

    add task

--- src/com/udesurd/Main.java
+++ src/com/udesurd/Main.java
@@ -14,6 +14,7 @@ public class Main {
 //      Ветвления:
         task2_1();
         task2_2();
+        task2_3();
     }
 
 
@@ -40,20 +41,16 @@ public class Main {
 //  Задание Второе:
     private static void task1_2() {
         System.out.println("2. Вычислить значение выражения по формуле (все переменные принимают действительные значения)");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Пожалуйста, введите ваше Первое число: ");
-        double a = scanner.nextInt();
-        System.out.println("Пожалуйста, введите ваше Второе число: ");
-        double b = scanner.nextInt();
-        System.out.println("Пожалуйста, введите ваше Третье число: ");
-        double c = scanner.nextInt();
+        double a = 6.0;
+        double b = 0.2;
+        double c = 10.0;
 //      Математическая формула и ответ:
         double res = (b + Math.sqrt(b * b + 4 * a * c)) / (2 * a) - Math.pow(a, 3) * c + Math.pow(b, -2);
         System.out.println("Ответ: " + res);
     }
 //  Задание Третье:
     private static void task1_3() {
-        System.out.println("Вычислить значение выражения по формуле (все переменные принимают действительные значения)");
+        System.out.println("3. Вычислить значение выражения по формуле (все переменные принимают действительные значения)");
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите 'x': ");
         double x = scanner.nextInt();
@@ -105,7 +102,6 @@ public class Main {
             System.out.println("Непрямоугольный.");
     }else
         System.out.println("Не существует.");
-
 }
 //  Задание Второе:
     private static void task2_2(){
@@ -118,5 +114,19 @@ public class Main {
         int res = Math.max(Math.min(a, b), Math.min(c,d));
         System.out.println("Ответ: " + res);
     }
+//  Задание Третье:
+private static void task2_3() {
+    System.out.println("3.  Даны три точки А(х1,у1), В(х2,у2) и С(х3,у3). Определить, будут ли они расположены на одной прямой.");
+    int x1 = 1;
+    int y1 = 3;
+    int x2 = 5;
+    int y2 = 0;
+    int x3 = 5;
+    int y3 = 0;
 
+    if ((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3) == 0)
+        System.out.println("На одной прямой");
+    else
+        System.out.println("Не на одной прямой");
+}
 }
